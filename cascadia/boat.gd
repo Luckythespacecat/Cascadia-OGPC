efc49911260bd68db1a8810397cc4402e0f58633
MiddleName144 <middlename144@gmail.com>
2025-02-27 10:53:36 -0800

    nothing

--- cascadia/boat.gd
+++ cascadia/boat.gd
@@ -1,1 +1,36 @@
 extends Node2D
+
+var rand
+
+func _ready():
+	$Timer.start()
+
+func _process(delta: float) -> void:
+	BoatToPlayer()
+
+func BoatToPlayer1():
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", Global.PlayerPos, 1).set_trans(Tween.TRANS_LINEAR)
+		
+func BoatToPlayer2():
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", Global.PlayerPos, 5).set_trans(Tween.TRANS_LINEAR)
+
+func BoatToPlayer():
+	if rand == 1:
+		BoatToPlayer1()
+	if rand == 0:
+		BoatToPlayer2()
+
+func PlayerExited():
+	pass
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	PlayerExited()
+	
+func randomgen():
+	rand = randi_range(0, 1)
+
+
+func _on_timer_timeout() -> void:
+	randomgen()
