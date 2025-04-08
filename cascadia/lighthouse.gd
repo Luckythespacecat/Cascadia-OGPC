c7aa9b67168c8f786c6bea5678c48480040ea469
besserwisser314 <besserwisser314@gmail.com>
2025-04-08 16:52:28 -0700

    yeah

--- cascadia/lighthouse.gd
+++ cascadia/lighthouse.gd
@@ -11,3 +11,7 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_released("Down") or Input.is_action_just_released("Up") or Input.is_action_just_released("Left") or Input.is_action_just_released("Right") :
 		pass
 		#$AnimatedSprite2D.
+	for i in $Area2D2.get_overlapping_areas():
+		if(i.get_collision_layer() == 2):
+			print("HEY IT WORKED")
+		pass
