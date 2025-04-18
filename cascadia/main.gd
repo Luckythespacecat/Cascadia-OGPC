1b7e1df465d783c4fba9f8c1964a3111894c9074
Luckythespacecat <103975833+Luckythespacecat@users.noreply.github.com>
2025-04-18 11:05:02 -0700

    aekjfdfaaufweougbfewa

--- cascadia/main.gd
+++ cascadia/main.gd
@@ -1,1 +1,24 @@
 extends Node2D
+
+func wood():
+	const woodResource = preload("res://wood.tscn")
+	get_tree().add_child(woodResource)
+	
+	var thing = Timer.new()
+	thing.wait_time = 3
+	thing.autostart
+
+func spawnWood():
+	var botY = $CanvasLayer/UserInterface/bottomRight.global_position.y
+	var topY = $CanvasLayer/UserInterface/topLeft.global_position.y
+	var botX = $CanvasLayer/UserInterface/bottomRight.global_position.x
+	var topX = $CanvasLayer/UserInterface/topLeft.global_position.x
+	
+	#if thing.timeout == true :
+		#wood()
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
