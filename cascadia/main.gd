c216d3e426e887d67c497261f15ac679ac6e5b91
Luckythespacecat <103975833+Luckythespacecat@users.noreply.github.com>
2025-04-19 07:16:13 -0700

    aefeae

--- cascadia/main.gd
+++ cascadia/main.gd
@@ -1,24 +1,31 @@
 extends Node2D
 
-func wood():
-	const woodResource = preload("res://wood.tscn")
-	get_tree().add_child(woodResource)
-	
-	var thing = Timer.new()
-	thing.wait_time = 3
-	thing.autostart
+var botY = 0
+var topY = 0
+var botX = 0
+var topX = 0
 
-func spawnWood():
+const woodResource = preload("res://wood.tscn")
+
+func wood():
+	
+	var woodInstance = woodResource.instantiate()
+	woodInstance.global_position.y = 0
+	woodInstance.global_position.x = 0
+
+func spawnWwood():
 	var botY = $CanvasLayer/UserInterface/bottomRight.global_position.y
 	var topY = $CanvasLayer/UserInterface/topLeft.global_position.y
 	var botX = $CanvasLayer/UserInterface/bottomRight.global_position.x
 	var topX = $CanvasLayer/UserInterface/topLeft.global_position.x
-	
-	#if thing.timeout == true :
-		#wood()
+	wood()
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_wood_timer_timeout() -> void:
+	wood()
