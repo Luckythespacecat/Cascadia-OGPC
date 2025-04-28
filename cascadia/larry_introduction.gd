1eb25ab0dd12b297842d821805e3414a5212db09
MiddleName144 <middlename144@gmail.com>
2025-04-28 09:50:08 -0700

    hey

--- cascadia/larry_introduction.gd
+++ cascadia/larry_introduction.gd
@@ -25,12 +25,12 @@ func _process(delta: float) -> void:
 	Dialoguemanager.start_dialogue( Global.textPos, [
 	"  Hey you, its about time you woke up  ",
 		"  The names Larry  ",
-		"  Larry the lamprey that is  ",
-		"  You're one of the only survivors  ",
+		"  Larry the Lamprey, that is!  ",
+		"  You're one of the only survivors.  ",
 		"  From what?  ",
-		"  You must've got hit hard  ",
-		"  I guess I'll help you around  ",
-		"  Even though i'm most likely...  ",
+		"  You must've got hit hard.  ",
+		"  I guess I'll help you around,  ",
+		"  even though I'm most likely...  ",
 		"  a figment of your imagination!  ",
 		"   ",
 		"   "
