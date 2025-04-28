d59005eb0b233b4f8124c8451885c4c187e808f5
Luckythespacecat <103975833+Luckythespacecat@users.noreply.github.com>
2025-04-27 21:29:19 -0700

    eee

--- cascadia/hunger_bar.gd
+++ cascadia/hunger_bar.gd
@@ -3,9 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass # Replace with function body
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	Global.Hunger -= 1
 	$Fill.size.x = int(lerp(int($Fill.size.x), int(Global.Hunger), delta ))
