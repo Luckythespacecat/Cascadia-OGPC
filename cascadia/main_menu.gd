d0db0a9ed2dc9dd5329af03a5edb2962ad535120
besserwisser314 <besserwisser314@gmail.com>
2025-04-28 23:55:12 -0700

    sss

--- cascadia/main_menu.gd
+++ cascadia/main_menu.gd
@@ -3,7 +3,7 @@ extends Node
 var TimeOut1 = 0
 var SkyFinalPos = Vector2(900.0, 500.0)
 
-# Called when the node enters the scene tree for the first time.
+# These variables and functions are to animate the elements of the main menu.
 func _ready() -> void:
 	
 	var Seatween = get_tree().create_tween()
@@ -19,17 +19,26 @@ func _ready() -> void:
 	NametweenPos.tween_property($Name, "position", Vector2(1100.0, 70.0), 3).set_trans(Tween.TRANS_QUART)
 
 	var PlayButtontweenPos = get_tree().create_tween()
-	PlayButtontweenPos.tween_property($Icon, "position", Vector2(1150.0, 550.0), 3).set_trans(Tween.TRANS_QUART)
+	PlayButtontweenPos.tween_property($Icon, "position", Vector2(1150.0, 490.0), 3).set_trans(Tween.TRANS_QUART)
 
+	var CreditsButtonPos = get_tree().create_tween()
+	CreditsButtonPos.tween_property($Icon2, "position", Vector2(1700.0, 940.0), 3).set_trans(Tween.TRANS_QUART)
 
+	var SettingsButtonPos = get_tree().create_tween()
+	SettingsButtonPos.tween_property($Icon3, "position", Vector2(1520.0, 540.0), 3).set_trans(Tween.TRANS_QUART)
+
+#Initiates the title's animation
 	$Name/StretchOut.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var MouseRelativePosition = get_viewport().get_mouse_position() + SkyFinalPos
+#This creates the background effect of moving opposite of the mouse. The TimeOut1 variable is to track when the
+#sky is in position.
+	var MouseRelativePosition = -1*(get_viewport().get_mouse_position() + SkyFinalPos)
 	if TimeOut1 == 1:
-		$Sky.global_position = SkyFinalPos - SkyFinalPos.lerp(MouseRelativePosition, delta)
+		$Sky.global_position = SkyFinalPos.lerp(MouseRelativePosition, delta)
 	pass
+#Starts the wave's animation as well as continueing the title's
 func _on_stretch_out_timeout() -> void:
 	TimeOut1 = 1
 	$Name/AnimationPlayer.play("Stretch")
@@ -50,6 +59,6 @@ func SlowTweens() :
 	var Landtweenlong = get_tree().create_tween()
 	Landtweenlong.tween_property($Sea, "position", Vector2(381.0, 400), 30).set_trans(Tween.TRANS_QUART)
 
-
+#The function that changes scenes when the play button is clicked.
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://LarryIntroduction.tscn")
