bd860b2657431b504d857feed8d2340d4c3d2e47
MiddleName144 <middlename144@gmail.com>
2025-05-02 10:56:34 -0700

    FineNathanYouMonster

--- cascadia/car.gd
+++ cascadia/car.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 @onready var CarArea = $CarArea
+@onready var flip1
 
 func _ready():
 	$CarArea/AnimatedSprite2D.play()
 	CarArea.area_entered.connect(_on_area_entered)
+	flip()
 
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "Area2D":
@@ -12,4 +14,12 @@ func _on_area_entered(area: Area2D) -> void:
 		Global.boatDirection = 0
 		
 	if area.name == "FootArea" or area.name == "HeadArea" or area.name == "BodyArea":
+		pass
+
+
+func flip():
+	flip1 = randi_range(1,10)
+	if flip1 <= 5:
+		$CarArea/AnimatedSprite2D.flip_h = true
+	else:
 		pass
